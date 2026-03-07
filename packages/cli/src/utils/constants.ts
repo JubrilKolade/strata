66567a01b607949e3b8723806164698f30fe8be3
JubrilKolade <jubrilkolade97@gmail.com>
2026-03-07 21:25:46 +0100

    chore(cli): define core constants and registry URLs

---
+++ packages/cli/src/utils/constants.ts
@@ -0,0 +1,13 @@
+export const DEFAULT_REGISTRY_URL = "https://raw.githubusercontent.com/yourusername/strata-ui/main/registry";
+export const DEFAULT_COMPONENTS_URL = "https://raw.githubusercontent.com/yourusername/strata-ui/main/src/components/ui";
+
+export const DEPENDENCIES = [
+    'nativewind@^4.0.0',
+    'react-native-reanimated@^3.6.0',
+];
+
+export const DEV_DEPENDENCIES = [
+    'tailwindcss@^3.4.0',
+    'clsx',
+    'tailwind-merge',
+];
