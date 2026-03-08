f9e330f52a5c8b3110fa5187a8eb008b46cb14a7
JubrilKolade <jubrilkolade97@gmail.com>
2026-03-08 21:45:08 +0100

    feat(ui): add accessible label component for form fields

---
+++ src/components/ui/label.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, type TextProps } from 'react-native';
+import { cn } from '../../lib/utils';
+
+const Label = React.forwardRef<React.ElementRef<typeof Text>, TextProps>(
+  ({ className, ...props }, ref) => (
+    <Text
+      ref={ref}
+      className={cn("text-sm font-medium text-foreground", className)}
+      {...props}
+    />
+  )
+);
+
+Label.displayName = 'Label';
+
+export { Label };
